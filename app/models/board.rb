d73baedfdfde03d6185e4477f0b806c37f983625
David Egbert <david.egbert@joinroot.com>
2020-04-10 14:08:51 -0400

    Add ability to print board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,25 +1,60 @@
 class Board
 
+  attr_reader :board
+
   def initialize
-    board = Array.new(8) { Array.new(8) }
-    board.each_with_index do |row, row_number|
-      row.each_with_index do |square, col_number|
+    @board = Array.new(8) { Array.new(8) }
+    @board.map!.with_index do |row, row_number|
+      row.map!.with_index do |square, col_number|
         if [1, 6].include?(row_number)
-          board[row_number][col_number] = "p"
+          square = "P"
         elsif [0, 7].exclude?(row_number)
-          board[row_number][col_number] = ""
+          square = " "
         elsif [0, 7].include?(col_number)
-          board[row_number][col_number] = "R"
+          square = "R"
         elsif [1, 6].include?(col_number)
-          board[row_number][col_number] = "N"
+          square = "N"
         elsif [2, 5].include?(col_number)
-          board[row_number][col_number] = "B"
+          square = "B"
         elsif 3 == col_number
-          board[row_number][col_number] = "Q"
+          square = "Q"
         elsif 4 == col_number
-          board[row_number][col_number] = "K"
+          square = "K"
         end
       end
     end
   end
+
+
+  def pretty_print
+    board_str = ""
+    board.each_with_index do |row, row_number|
+      board_str += _to_external_rank(:rank => row_number).to_s
+      row.each_with_index do |square, col_number|
+        board_str += " #{square}"
+      end
+      board_str += "\n"
+    end
+    board_str += " "
+    (0...board.length).each { |index| board_str += " #{_to_external_file(:file => index)}" }
+    puts board_str
+    board_str
+  end
+
+  def _to_internal_rank(rank:)
+    8 - rank
+  end
+ 
+  def _to_internal_file(file:)
+    ("A".."Z").to_a.find_index(file.upcase) + 1
+  end
+  
+  def _to_external_rank(rank:)
+    _to_internal_rank(:rank => rank)
+  end
+
+  def _to_external_file(file:)
+   ("A".."Z").to_a[file]
+  end
+
 end
