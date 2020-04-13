1427d0730bf998f17f06c9cde5637b34fc9215fe
David Egbert <david.egbert@joinroot.com>
2020-04-13 14:49:35 -0400

    Add more rook tests

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -4,17 +4,17 @@ module Pieces
       super(:side => side, :type => "R")
     end
 
-    def path_to_target(start_row:, start_column:, end_row:, end_column:)
-      path = (start_row..end_row).flat_map do |row|
-        (start_column..end_column).flat_map do |column|
+    def path_to_target(ply:)
+      path = ([ply.start_row, ply.end_row].min..[ply.start_row, ply.end_row].max).flat_map do |row|
+        ([ply.start_column, ply.end_column].min..[ply.start_column, ply.end_column].max).flat_map do |column|
           [OpenStruct.new(:row => row, :column => column)]
         end
       end
       path[1..-2]
     end
 
-    def can_move_to_target(start_row:, start_column:, end_row:, end_column:)
-      (start_row == end_row) ^ (start_column == end_column)
+    def can_move_to_target(ply:)
+      (ply.start_row == ply.end_row) ^ (ply.start_column == ply.end_column)
     end
   end
 end
--- spec/models/pieces/rook_spec.rb
+++ spec/models/pieces/rook_spec.rb
@@ -1,24 +1,50 @@
 require "rails_helper"
 
 RSpec.describe Pieces::Rook do
-  describe ".can_move_to_target" do
-    let(:rook) { described_class.new(:side => "white") }
+  let(:rook) { described_class.new(:side => "white") }
 
+  describe ".can_move_to_target" do
     context "when the rook moves in along a row" do
       it "returns true" do
-        expect(rook.can_move_to_target(:start_row => 2, :start_column => 2, :end_row => 2, :end_column => 5)).to eql(true)
+        expect(rook.can_move_to_target(:ply => Ply.new(:move => "c3f3"))).to eql(true)
       end
     end
 
     context "when the rook moves in along a column" do
       it "returns true" do
-        expect(rook.can_move_to_target(:start_row => 2, :start_column => 2, :end_row => 5, :end_column => 2)).to eql(true)
+        expect(rook.can_move_to_target(:ply => Ply.new(:move => "c3c6"))).to eql(true)
       end
     end
 
     context "when the rook moves in along a row and a column" do
       it "returns false" do
-        expect(rook.can_move_to_target(:start_row => 2, :start_column => 2, :end_row => 5, :end_column => 5)).to eql(false)
+        expect(rook.can_move_to_target(:ply => Ply.new(:move => "c3f6"))).to eql(false)
+      end
+    end
+  end
+
+  describe ".path_to_target" do
+    context "when the rook moves in along a row" do
+      it "returns a list of spaces in its path" do
+        expect(rook.path_to_target(:ply => Ply.new(:move => "c3f3"))).to match_array(
+          [OpenStruct.new(:row => 5, :column => 3),
+           OpenStruct.new(:row => 5, :column => 4)]
+        )
+      end
+    end
+
+    context "when the rook moves in along a column" do
+      it "returns a list of spaces in its path" do
+        expect(rook.path_to_target(:ply => Ply.new(:move => "c3c6"))).to match_array(
+          [OpenStruct.new(:row => 4, :column => 2),
+           OpenStruct.new(:row => 3, :column => 2)]
+        )
+      end
+    end
+
+    context "when the rook moves one space" do
+      it "returns an empty array" do
+        expect(rook.path_to_target(:ply => Ply.new(:move => "c3c4"))).to eql([])
       end
     end
   end
