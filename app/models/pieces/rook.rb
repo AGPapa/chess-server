221952074755e63ed6cad7bb3ad15cf327bbc086
David Egbert <david.egbert@joinroot.com>
2020-04-13 16:31:46 -0400

    rook spec changes

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -6,8 +6,8 @@ module Pieces
 
     def path_to_target(ply:)
       path = ([ply.start_row, ply.end_row].min..[ply.start_row, ply.end_row].max).flat_map do |row|
-        ([ply.start_column, ply.end_column].min..[ply.start_column, ply.end_column].max).flat_map do |column|
-          [OpenStruct.new(:row => row, :column => column)]
+        ([ply.start_column, ply.end_column].min..[ply.start_column, ply.end_column].max).map do |column|
+          OpenStruct.new(:row => row, :column => column)
         end
       end
       path[1..-2]
--- spec/models/pieces/rook_spec.rb
+++ spec/models/pieces/rook_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Pieces::Rook do
+  include LocationInterpreter
   let(:rook) { described_class.new(:side => "white") }
 
   describe ".can_move_to_target" do
@@ -27,8 +28,8 @@ RSpec.describe Pieces::Rook do
     context "when the rook moves in along a row" do
       it "returns a list of spaces in its path" do
         expect(rook.path_to_target(:ply => Ply.new(:move => "c3f3"))).to match_array(
-          [OpenStruct.new(:row => 5, :column => 3),
-           OpenStruct.new(:row => 5, :column => 4)]
+          [OpenStruct.new(:row => to_internal_row(:rank => 3), :column => to_internal_column(:file => "d")),
+           OpenStruct.new(:row => to_internal_row(:rank => 3), :column => to_internal_column(:file => "e"))]
         )
       end
     end
@@ -36,8 +37,8 @@ RSpec.describe Pieces::Rook do
     context "when the rook moves in along a column" do
       it "returns a list of spaces in its path" do
         expect(rook.path_to_target(:ply => Ply.new(:move => "c3c6"))).to match_array(
-          [OpenStruct.new(:row => 4, :column => 2),
-           OpenStruct.new(:row => 3, :column => 2)]
+          [OpenStruct.new(:row => to_internal_row(:rank => 4), :column => to_internal_column(:file => "c")),
+           OpenStruct.new(:row => to_internal_row(:rank => 5), :column => to_internal_column(:file => "c"))]
         )
       end
     end
